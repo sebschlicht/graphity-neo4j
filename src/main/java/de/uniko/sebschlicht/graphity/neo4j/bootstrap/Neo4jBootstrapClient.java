8a193493be411011e60c3d78107e0ed13697e8e6
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2015-03-04 17:17:57 +0100

    Neo4j bootstrap API doc, file loader added

--- src/main/java/de/uniko/sebschlicht/graphity/neo4j/bootstrap/Neo4jBootstrapClient.java
+++ src/main/java/de/uniko/sebschlicht/graphity/neo4j/bootstrap/Neo4jBootstrapClient.java
@@ -2,18 +2,26 @@ package de.uniko.sebschlicht.graphity.neo4j.bootstrap;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 import org.neo4j.unsafe.batchinsert.BatchInserter;
 import org.neo4j.unsafe.batchinsert.BatchInserters;
 
 import de.uniko.sebschlicht.graphity.bootstrap.BootstrapClient;
 import de.uniko.sebschlicht.graphity.bootstrap.User;
+import de.uniko.sebschlicht.graphity.bootstrap.UserManager;
+import de.uniko.sebschlicht.graphity.bootstrap.generate.MutableState;
+import de.uniko.sebschlicht.graphity.bootstrap.load.BootstrapLoader;
 import de.uniko.sebschlicht.graphity.neo4j.EdgeType;
 import de.uniko.sebschlicht.graphity.neo4j.NodeType;
 import de.uniko.sebschlicht.graphity.neo4j.model.StatusUpdateProxy;
 import de.uniko.sebschlicht.graphity.neo4j.model.UserProxy;
+import de.uniko.sebschlicht.socialnet.Subscription;
 
 public class Neo4jBootstrapClient extends BootstrapClient {
 
@@ -28,9 +36,23 @@ public class Neo4jBootstrapClient extends BootstrapClient {
         _inserter.shutdown();
     }
 
+    /**
+     * Bulk loads a social network state into the news feed service storage.
+     * The social network state is handled by the Neo4j server plugin REST API.
+     * This method has to load all the data in a single call.
+     * 
+     * @param userIds
+     *            identifiers of all users existing
+     * @param subscriptions
+     *            array with following content schema:<br>
+     *            numSubscriptions, idFollowed_0 .. idFollowed_numSubscriptions<br>
+     *            per user, where users are sorted as in userIds
+     * @param numPosts
+     *            number of posts per user, where users are sorted as in userIds
+     */
     public void bootstrap(long[] userIds, long[] subscriptions, int[] numPosts) {
-        createUsers(userIds);
-        System.out.println(userIds.length + " users created.");
+        long numUsers = createUsers(userIds);
+        System.out.println(numUsers + " users created.");
 
         // subscribe users
         long numSubscriptions = createSubscriptions(subscriptions);
@@ -39,21 +61,31 @@ public class Neo4jBootstrapClient extends BootstrapClient {
         // create and link posts
         long numPostsTotal = createPosts(numPosts);
         System.out.println(numPostsTotal + " posts created.");
-        linkPosts(numPosts);
-        System.out.println("posts linked.");
+        numPostsTotal = linkPosts(numPosts);
+        System.out.println(numPostsTotal + " posts linked.");
     }
 
-    protected void createUsers(long[] userIds) {
+    /**
+     * Adds the users to the database.
+     * 
+     * @param userIds
+     *            identifiers of all users existing
+     */
+    protected long createUsers(long[] userIds) {
+        long numUsers = 0;
         User user;
         Map<String, Object> userProperties;
         long nodeId;
+        _users.setUserIds(userIds);
         for (long id : userIds) {
-            user = _users.getUser(id);
+            user = _users.addUser(id);
             userProperties = new HashMap<>();
             userProperties.put(UserProxy.PROP_IDENTIFIER, String.valueOf(id));
             nodeId = _inserter.createNode(userProperties, NodeType.USER);
             user.setNodeId(nodeId);
+            numUsers += 1;
         }
+        return numUsers;
     }
 
     /**
@@ -82,8 +114,8 @@ public class Neo4jBootstrapClient extends BootstrapClient {
                     followed = _users.getUser(idFollowed);
                     _inserter.createRelationship(user.getNodeId(),
                             followed.getNodeId(), EdgeType.FOLLOWS, null);
-                    numSubscriptions += 1;
                 }
+                numSubscriptions += numUserSubscriptions;
             } else {// ReadOptimizedGraphity
                 throw new IllegalStateException("can not subscribe");
             }
@@ -96,7 +128,7 @@ public class Neo4jBootstrapClient extends BootstrapClient {
      * 
      * @param numPosts
      *            number of posts per user, where users are sorted as in userIds
-     * @return number of posts
+     * @return number of posts created
      */
     protected long createPosts(int[] numPosts) {
         long numTotalPosts = 0;
@@ -128,6 +160,36 @@ public class Neo4jBootstrapClient extends BootstrapClient {
             // update last_post
             _inserter.setNodeProperty(user.getNodeId(),
                     UserProxy.PROP_LAST_STREAM_UDPATE, tsLastPost);
+        }
+        return numTotalPosts;
+    }
+
+    /**
+     * Links the posts in the database.
+     * 
+     * @param numPosts
+     *            number of posts per user, where users are sorted as in userIds
+     * @return number of posts linked
+     */
+    protected long linkPosts(int[] numPosts) {
+        long numTotalPosts = 0;
+        User user;
+        for (int i = 0; i < numPosts.length; ++i) {
+            user = _users.getUserByIndex(i);
+            if (user.getPostNodeIds() == null) {
+                continue;
+            }
+            long[] postNodeIds = user.getPostNodeIds();
+            for (int iPost = 0; iPost < postNodeIds.length; ++iPost) {
+                if (iPost + 1 < postNodeIds.length) {// newerPost -> olderPost
+                    _inserter.createRelationship(postNodeIds[iPost + 1],
+                            postNodeIds[iPost], EdgeType.PUBLISHED, null);
+                } else {// user -> newestPost
+                    _inserter.createRelationship(user.getNodeId(),
+                            postNodeIds[iPost], EdgeType.PUBLISHED, null);
+                }
+            }
+            numTotalPosts += postNodeIds.length;
         }
         return numTotalPosts;
     }
@@ -195,14 +257,15 @@ public class Neo4jBootstrapClient extends BootstrapClient {
         return numTotalPosts;
     }
 
-    protected void linkPosts(int[] numPosts) {
-        User user;
-        for (int i = 0; i < numPosts.length; ++i) {
-            user = _users.getUserByIndex(i);
-            if (user.getPostNodeIds() == null) {
+    @Override
+    protected long linkPosts() {
+        long numTotalPosts = 0;
+        for (User user : _users.getUsers()) {
+            long[] postNodeIds = user.getPostNodeIds();
+            if (postNodeIds == null) {// should not happen
                 continue;
             }
-            long[] postNodeIds = user.getPostNodeIds();
+
             for (int iPost = 0; iPost < postNodeIds.length; ++iPost) {
                 if (iPost + 1 < postNodeIds.length) {// newerPost -> olderPost
                     _inserter.createRelationship(postNodeIds[iPost + 1],
@@ -212,30 +275,90 @@ public class Neo4jBootstrapClient extends BootstrapClient {
                             postNodeIds[iPost], EdgeType.PUBLISHED, null);
                 }
             }
+            numTotalPosts += postNodeIds.length;
         }
+        return numTotalPosts;
     }
 
-    @Override
-    protected void linkPosts() {
-        for (User user : _users.getUsers()) {
-            if (user.getNumPosts() == 0) {// should not happen
-                continue;
+    private void neo4jBootstrap(File fBootstrapLog) throws IOException {
+        BootstrapLoader bootstrapLoader =
+                new BootstrapLoader(fBootstrapLog, true);
+        MutableState state = bootstrapLoader.getState();
+        _users = new UserManager();
+
+        TreeSet<Long> sUserIds = new TreeSet<>();
+        ArrayList<Long> aUserIds = new ArrayList<>();
+
+        // convert from mutable to final social network state
+        // Note: This will happen on the client machine!
+        long prevUserId = -1, userId = 0;
+        ArrayList<Long> aSubscriptions = new ArrayList<>();
+        List<Long> userSubs = new LinkedList<>();
+
+        // load subscriptions and followers
+        TreeSet<Subscription> subscriptions = state.getSubscriptions();
+        int iSubscription = 0;
+        int numSubscriptions = subscriptions.size();
+        for (Subscription subscription : subscriptions) {
+            // switch to current user
+            userId = subscription.getIdSubscriber();
+            if (userId != prevUserId) {
+                // make previous user persistent when switching to a new user
+                aSubscriptions.add((long) userSubs.size());
+                for (long userSub : userSubs) {
+                    aSubscriptions.add(userSub);
+                }
+                // switch to new user
+                sUserIds.add(userId);
+                aUserIds.add(userId);
+                userSubs.clear();
+                prevUserId = userId;
             }
-            long[] postNodeIds = user.getPostNodeIds();
-            for (int iPost = 0; iPost < postNodeIds.length; ++iPost) {
-                if (iPost + 1 < postNodeIds.length) {// newerPost -> olderPost
-                    _inserter.createRelationship(postNodeIds[iPost + 1],
-                            postNodeIds[iPost], EdgeType.PUBLISHED, null);
-                } else {// user -> newestPost
-                    _inserter.createRelationship(user.getNodeId(),
-                            postNodeIds[iPost], EdgeType.PUBLISHED, null);
+
+            // add subscription for current user
+            userSubs.add(subscription.getIdFollowed());
+
+            // make persistent if last user
+            if (iSubscription == numSubscriptions - 1) {
+                aSubscriptions.add((long) userSubs.size());
+                for (long userSub : userSubs) {
+                    aSubscriptions.add(userSub);
                 }
             }
+            iSubscription += 1;
         }
+
+        // load posts and authors
+        ArrayList<Integer> aNumPosts = new ArrayList<Integer>();
+        Map<Long, int[]> numPosts = state.getNumPosts();
+        for (long crrUserId : aUserIds) {// load posts from users with active subscriptions
+            int[] numUserPosts = numPosts.get(crrUserId);
+            aNumPosts.add(numUserPosts[1]);// total number of posts // we are bootstrapping!
+        }
+        for (Map.Entry<Long, int[]> entry : numPosts.entrySet()) {
+            userId = entry.getKey();
+            if (sUserIds.contains(userId)) {// user with active subscriptions
+                continue;
+            }
+            int[] numUserPosts = entry.getValue();
+            aUserIds.add(userId);
+            aNumPosts.add(numUserPosts[1]);// total number of posts // we are bootstrapping!
+        }
+
+        long[] userIds = listToArray(aUserIds);
+        long[] arraySubscriptions = listToArray(aSubscriptions);
+        int[] arrayNumPosts = new int[aNumPosts.size()];
+        int i = 0;
+        for (int value : aNumPosts) {
+            arrayNumPosts[i] = value;
+            i += 1;
+        }
+
+        bootstrap(userIds, arraySubscriptions, arrayNumPosts);
     }
 
     public static void main(String[] args) throws IOException {
-        File fDatabase = new File("/media/shared/neoboot");
+        File fDatabase = new File("/media/shared/neobootfile");
         File fBootstrapLog = new File("bootstrap.log");
         final Neo4jBootstrapClient bootstrapClient =
                 new Neo4jBootstrapClient(fDatabase.getAbsolutePath());
